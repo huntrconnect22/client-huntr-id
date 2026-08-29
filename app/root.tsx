5efcc2963b55adcbb78838b689a9c835e9103557
Wildan Belfiore <mikewildan@outlook.com>
2026-08-29 14:36:29 +0700

    add suppressHydrationWarning

--- app/root.tsx
+++ app/root.tsx
@@ -69,7 +69,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Meta />
         <Links />
       </head>
-      <body>
+      <body suppressHydrationWarning>
         <ThemeProvider>
           <EventBusProvider>
             {children}
